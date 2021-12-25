1525d9ef3e47da078548cc33aa839a028916fecd
Tim Jedrek <tim@timjedrek.com>
2021-12-24 23:16:31 -0600

    implemented bootstrap

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # rock-paper-scissors
-added styling
-12-24-2021 T. Santos
+added styling using bootstrap 12-24-2021 T. Santos
+
+Summary
+- This is the first time I tried using bootstrap to design a UI
+- Not mobile friendly
+    - bootstrap 12 column grid forces UI to break for game
+
+What I learned?
+- Don't design game UIs with bootstrap.
+    - bootstrap works goods for content but not game UI
+- Bootstrap class system is overconvuluted and opinionated
+- Designing your own flexbox grid layout works better and is more unopinionated
--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function playRound(humanAnswer) {
     //Tie
     if (humanAnswer == computerAnswer) {
         roundWinner = "tie"
-        document.getElementById("status-area-box").innerHTML = `You chose ${humanAnswer} and the computer chose ${computerAnswer}. <h5>It's a tie<h5>`;
+        document.getElementById("status-area-box").innerHTML = `人間は${translate(humanAnswer)}。コンピュータは${translate(computerAnswer)}。<br>今回は引き分け`;
     }
 
     //Human Win
@@ -37,7 +37,7 @@ function playRound(humanAnswer) {
         roundWinner = "human";
         humanScore ++;
         document.getElementById("human-score-number").innerHTML = humanScore;
-        document.getElementById("status-area-box").innerHTML = `You chose ${humanAnswer} and the computer chose ${computerAnswer}. <h5>Human wins this round<h5>`;
+        document.getElementById("status-area-box").innerHTML = `人間は${translate(humanAnswer)}。コンピュータは${translate(computerAnswer)}。<br>今回は人間が勝った`;
     }
 
     //Computer Win
@@ -45,7 +45,7 @@ function playRound(humanAnswer) {
         roundWinner = "computer";
         computerScore ++;
         document.getElementById("computer-score-number").innerHTML = computerScore;
-        document.getElementById("status-area-box").innerHTML = `You chose ${humanAnswer} and the computer chose ${computerAnswer}. <h5>Computer wins this round<h5>`;
+        document.getElementById("status-area-box").innerHTML = `人間は${translate(humanAnswer)}。コンピュータは${translate(computerAnswer)}。<br>今回はコンピュータが勝った`;
     }
     
     scoreCounter();
@@ -60,15 +60,15 @@ function playRound(humanAnswer) {
 function scoreCounter() {
     if (humanScore >= 3) {
         winner = "human";
-        document.getElementById("status-area-box").innerHTML = `Human is the winner of the game. play again?`;
-        document.getElementById("user-selection").innerHTML = `<button onClick="bringBackSelection()">もう一回？</button>`
+        document.getElementById("status-area-box").innerHTML = `人間は成功です！`;
+        document.getElementById("user-selection").innerHTML = `<div class="col-sm-4 text-center"><button onClick="bringBackSelection()" class="btn btn-secondary btn-lg">もう一回？</button></div>`
         return ;
     }
 
     if (computerScore >= 3) {
         winner = "computer";
-        document.getElementById("status-area-box").innerHTML = `Computer is the winner of the game. Play again?`;
-        document.getElementById("user-selection").innerHTML = `<button onClick="bringBackSelection()">もう一回？</button>`
+        document.getElementById("status-area-box").innerHTML = `人間は失敗です！`;
+        document.getElementById("user-selection").innerHTML = `<div class="col-sm-4 text-center"><button onClick="bringBackSelection()" class="btn btn-secondary btn-lg">もう一回？</button></div>`
         return;
     }
 
@@ -88,9 +88,15 @@ function updateImage (humanAnswer, computerAnswer) {
 
 function bringBackSelection () {
     document.getElementById("user-selection").innerHTML = `
-    <button onclick="playRound('Rock')">石</button>
-    <button onClick="playRound('Paper')">紙</button>
-    <button onclick="playRound('Scissors')">鋏</button>
+    <div class="col-sm-4 text-center">
+        <button class="btn btn-secondary btn-lg" onclick="playRound('Rock')">石</button>
+    </div>
+    <div class="col-sm-4 text-center">
+        <button class="btn btn-secondary btn-lg" onClick="playRound('Paper')">紙</button>
+    </div>
+    <div class="col-sm-4 text-center">
+        <button class="btn btn-secondary btn-lg" onclick="playRound('Scissors')">鋏</button>
+    </div>
     `;
     reset();
     return;
@@ -102,7 +108,32 @@ function reset () {
     document.getElementById("computer-score-number").innerHTML = "0";
     document.getElementById("human-score-number").innerHTML = "0";
     document.getElementById("status-area-box").innerHTML = `では、開始しましょう`;
-    document.getElementById("selection-image-space-human").innerHTML = null;
-    document.getElementById("selection-image-space-computer").innerHTML = null;
+    document.getElementById("selection-image-space-human").innerHTML = `
+    <div class="image-container">
+        <img src="media/無い.png" alt="this is where the human hand goes">
+    </div>
+    `;
+    document.getElementById("selection-image-space-computer").innerHTML = `
+    <div class="image-container">
+        <img src="media/無い.png" alt="this is where the human hand goes">
+    </div>
+    `;
     return;
 }
+
+function translate(text) {
+    let translatedText;
+    if (text == "Rock") {
+        translatedText = "石";
+    }
+    else if (text == "Paper") {
+        translatedText = "紙";
+    }
+    else if (text == "Scissors") {
+        translatedText = "鋏";
+    }
+    else {
+        translatedText = "error"
+    }
+    return translatedText;
+} 
